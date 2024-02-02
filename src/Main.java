7ab1a0b92b280edc90ae3ea4f1ffff55d8521693
João Pedro A. Hypólito <jpazhy@gmail.com>
2024-02-02 12:18:48 -0300

    refactor-adiciona-enunciado
    Adiciona o enunciado do desafio proposto como comentário no código.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+/*
+- Crie uma classe Soma em Java para imprimir no console o resultado da operação 10 + 5.
+    Certifique-se de que o resultado seja exibido sem o uso de aspas, apresentando o valor "15" ao invés da expressão "10+5".
+- Crie uma classe Subtração para imprimir o resultado de 10 - 5 no console.
+ */
+
 public class Main {
     public static void main(String[] args) {
         int number1 = 10;
